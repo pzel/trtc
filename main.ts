926bafd3f16ee76d2ed0224d8ba7b7518ad8bff5
Simon Zelazny <zelazny@mailbox.org>
2025-10-28 18:08:23 +0100

    CH1 pp 6-7

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-export const EPSILON = 0.0001
+export const EPSILON = 0.0001;
 
 export class Tuple extends Float32Array {
-
   constructor(x: number, y: number, z: number, w: number) {
+    if (!((0.0 <= w) && (w <= 1.0))) {
+      throw new RangeError("W must be must be between 0 and 1.");
+    }
     super([x, y, z, w]);
   }
 
@@ -18,44 +20,67 @@ export class Tuple extends Float32Array {
   get w() {
     return this[3];
   }
-  get is_a_point() {
+  get is_a_point(): boolean {
     return Math.round(this[3]) == 1;
   }
-  get is_a_vector() {
+  get is_a_vector(): boolean {
     return Math.round(this[3]) == 0;
   }
 
-  tupleEqual(other: Tuple) {
+  tupleEqual(that: Tuple): boolean {
     return (
-      floatEqual(this.x, other.x) &&
-        floatEqual(this.y, other.y) &&
-        floatEqual(this.z, other.z) &&
-        floatEqual(this.w, other.w))
+      floatEqual(this.x, that.x) &&
+      floatEqual(this.y, that.y) &&
+      floatEqual(this.z, that.z) &&
+      floatEqual(this.w, that.w)
+    );
   }
 
+  plus(that: Tuple): Tuple {
+    return new Tuple(
+      this.x + that.x,
+      this.y + that.y,
+      this.z + that.z,
+      this.w + that.w,
+    );
+  }
+
+  minus(that: Tuple): Tuple {
+    return new Tuple(
+      this.x - that.x,
+      this.y - that.y,
+      this.z - that.z,
+      this.w - that.w,
+    );
+  }
+
+  negate(): Tuple {
+    return new Tuple(
+      -this.x,
+      -this.y,
+      -this.z,
+      -this.w,
+    );
+  }
 }
 
 export class Point extends Tuple {
-  constructor (x: number, y: number, z: number) {
-    super(x,y,z,1.0);
+  constructor(x: number, y: number, z: number) {
+    super(x, y, z, 1.0);
   }
 }
 
 export class Vector extends Tuple {
-  constructor (x: number, y: number, z: number) {
-    super(x,y,z,0.0);
+  constructor(x: number, y: number, z: number) {
+    super(x, y, z, 0.0);
   }
 }
 
-
-function floatEqual(a: number, b: number) : boolean {
-  return Math.abs(a-b) < EPSILON;
+function floatEqual(a: number, b: number): boolean {
+  return Math.abs(a - b) < EPSILON;
 }
-
 
 // // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 // if (import.meta.main) {
 //   console.log("Add 2 + 3 =", add(2, 3));
 // }
-
-
--- main_test.ts
+++ main_test.ts
@@ -1,91 +1,191 @@
 import { describe, it } from "@std/testing/bdd";
-import { assert, assertFalse, assertEquals, assertAlmostEquals } from "@std/assert";
-import { EPSILON, Tuple, Point, Vector } from "./main.ts";
+import {
+  assert,
+  assertAlmostEquals,
+  assertEquals,
+  assertFalse,
+  assertThrows,
+} from "@std/assert";
+import { EPSILON, Point, Tuple, Vector } from "./main.ts";
 
 describe("tuples.feature", () => {
-
   it("a tuple with w=1.0 is a point", () => {
     //given
-    const a = new Tuple(4.3, -4.2, 3.1, 1.0)
+    const a = new Tuple(4.3, -4.2, 3.1, 1.0);
     //then
-    assertAlmostEquals(a.x, 4.3, EPSILON)
-    assertAlmostEquals(a.y,-4.2, EPSILON)
-    assertAlmostEquals(a.z, 3.1, EPSILON)
-    assertAlmostEquals(a.w, 1.0, EPSILON)
-    assert(a.is_a_point)
-    assertFalse(a.is_a_vector)
+    assertAlmostEquals(a.x, 4.3, EPSILON);
+    assertAlmostEquals(a.y, -4.2, EPSILON);
+    assertAlmostEquals(a.z, 3.1, EPSILON);
+    assertAlmostEquals(a.w, 1.0, EPSILON);
+    assert(a.is_a_point);
+    assertFalse(a.is_a_vector);
   });
 
   it("a tuple with w=0.0 is a vector", () => {
     //given
-    const a = new Tuple(4.3, -4.2, 3.1, 0.0)
+    const a = new Tuple(4.3, -4.2, 3.1, 0.0);
     //then
-    assertAlmostEquals(a.x, 4.3, EPSILON)
-    assertAlmostEquals(a.y, -4.2, EPSILON)
-    assertAlmostEquals(a.z, 3.1, EPSILON)
-    assertAlmostEquals(a.w, 0.0, EPSILON)
-    assertFalse(a.is_a_point)
-    assert(a.is_a_vector)
+    assertAlmostEquals(a.x, 4.3, EPSILON);
+    assertAlmostEquals(a.y, -4.2, EPSILON);
+    assertAlmostEquals(a.z, 3.1, EPSILON);
+    assertAlmostEquals(a.w, 0.0, EPSILON);
+    assertFalse(a.is_a_point);
+    assert(a.is_a_vector);
   });
-
 
   it("a Point is a tuple with w=1", () => {
     //given
-    const a = new Point(1.1, 2.2, 3.3)
+    const a = new Point(1.1, 2.2, 3.3);
     //then
-    assert(a.tupleEqual(new Tuple(1.1, 2.2, 3.3, 1.0)))
+    assert(a.tupleEqual(new Tuple(1.1, 2.2, 3.3, 1.0)));
   });
 
   it("a Vector is a tuple with w=0", () => {
     //given
-    const a = new Vector(0.1, 0.2, 0.3)
+    const a = new Vector(0.1, 0.2, 0.3);
     //then
-    assert(a.tupleEqual(new Tuple(0.1, 0.2, 0.3, 0.0)))
+    assert(a.tupleEqual(new Tuple(0.1, 0.2, 0.3, 0.0)));
   });
-
 
   it("a Vector is not equal to a Point with the same coords", () => {
     //given
-    const v = new Vector(0.1, 0.2, 0.3)
-    const p = new Point(0.1, 0.2, 0.3)
+    const v = new Vector(0.1, 0.2, 0.3);
+    const p = new Point(0.1, 0.2, 0.3);
     //then
-    assertFalse(v.tupleEqual(p))
+    assertFalse(v.tupleEqual(p));
   });
 
   it("two Points are equal if they have the same components", () => {
     //given
-    const v1 = new Point(4, 5, 6)
-    const v2 = new Point(4, 5, 6)
+    const v1 = new Point(4, 5, 6);
+    const v2 = new Point(4, 5, 6);
     //then
-    assert(v1.tupleEqual(v2))
+    assert(v1.tupleEqual(v2));
   });
 
   it("two Points are NOT equal if they have differing components", () => {
     //given
-    const v1 = new Point(4, 5, 6.1)
-    const v2 = new Point(4, 5, 6)
+    const v1 = new Point(4, 5, 6.1);
+    const v2 = new Point(4, 5, 6);
     //then
-    assertFalse(v1.tupleEqual(v2))
+    assertFalse(v1.tupleEqual(v2));
   });
-
 
   it("two Vectors are equal if they have the same components", () => {
     //given
-    const v1 = new Vector(4, 5, 6)
-    const v2 = new Vector(4, 5, 6)
+    const v1 = new Vector(4, 5, 6);
+    const v2 = new Vector(4, 5, 6);
     //then
-    assert(v1.tupleEqual(v2))
+    assert(v1.tupleEqual(v2));
   });
 
   it("two Vectors are NOT equal if they have differing components", () => {
     //given
-    const v1 = new Vector(4, 5, 6.1)
-    const v2 = new Vector(4, 5, 6)
+    const v1 = new Vector(4, 5, 6.1);
+    const v2 = new Vector(4, 5, 6);
     //then
-    assertFalse(v1.tupleEqual(v2))
+    assertFalse(v1.tupleEqual(v2));
   });
 
+  it("two Tuples can be added", () => {
+    //given
+    const t1 = new Tuple(1, 2, 3, 0);
+    const t2 = new Tuple(4, 5, 6, 0);
+    //when
+    const t3 = t1.plus(t2);
 
+    //then
+    assert(t3.tupleEqual(new Tuple(5, 7, 9, 0)));
+  });
 
+  it("a Point can be added to a Vector", () => {
+    //given
+    const p = new Point(1, 2, 3);
+    const v = new Vector(4, 5, 6);
+    //when
+    const p2 = p.plus(v);
 
+    //then
+    assert(p2.tupleEqual(new Point(5, 7, 9)));
+  });
+
+  it("Point+Vector addition is commutative", () => {
+    //given
+    const p = new Point(1, 2, 3);
+    const v = new Vector(4, 5, 6);
+    //when
+    const pv = p.plus(v);
+    const vp = v.plus(p);
+
+    //then
+    assert(pv.tupleEqual(vp));
+  });
+
+  it("adding two Points raises an exception", () => {
+    //given
+    const p = new Point(1, 2, 3);
+    const q = new Point(4, 5, 6);
+    //then
+    assertThrows(() => p.plus(q), RangeError);
+  });
+
+  it("subtracts two points to produce a Vector", () => {
+    //given
+    const p1 = new Point(3, 2, 1);
+    const p2 = new Point(5, 6, 7);
+    //when
+    const p3 = p1.minus(p2);
+    //then
+    assert(p3.tupleEqual(new Vector(-2, -4, -6)));
+  });
+
+  it("subtracts two Vectors to produce a Vector", () => {
+    //given
+    const v1 = new Vector(3, 2, 1);
+    const v2 = new Vector(5, 6, 7);
+    //when
+    const v3 = v1.minus(v2);
+    //then
+    assert(v3.tupleEqual(new Vector(-2, -4, -6)));
+  });
+
+  it("subtracts a Vector from a Point to produce a Point", () => {
+    //given
+    const p = new Point(3, 2, 1);
+    const v = new Vector(5, 6, 7);
+    //when
+    const pv = p.minus(v);
+    //then
+    assert(pv.tupleEqual(new Point(-2, -4, -6)));
+  });
+
+  it("subtracting a Point from a Vector raises an exception", () => {
+    //given
+    const p = new Point(3, 2, 1);
+    const v = new Vector(5, 6, 7);
+    //when
+    assertThrows(() => v.minus(p), RangeError);
+  });
+
+  it("subtracting a Vector from the zero vector gives the negated vector", () => {
+    //given
+    const zero = new Vector(0, 0, 0);
+    const v = new Vector(1, -2, 3);
+    //when
+    assert(zero.minus(v).tupleEqual(new Tuple(-1, 2, -3, 0)));
+  });
+
+  it("negate() is the same as subtracting a Vector from the zero vector", () => {
+    //given
+    const v = new Vector(1, -2, 3);
+    //when
+    assert(v.negate().tupleEqual(new Tuple(-1, 2, -3, 0)));
+  });
+
+  it("negating a Point raises an exception", () => {
+    //given
+    const p = new Point(3, 2, 1);
+    //when
+    assertThrows(() => p.negate(), RangeError);
+  });
 });
