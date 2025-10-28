c4952fcfc673b4a8825af85a42e62a84e39a2f82
Simon Zelazny <zelazny@mailbox.org>
2025-10-28 17:31:44 +0100

    CH1 pp 4-5

--- main.ts
+++ main.ts
@@ -1,8 +1,61 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export const EPSILON = 0.0001
+
+export class Tuple extends Float32Array {
+
+  constructor(x: number, y: number, z: number, w: number) {
+    super([x, y, z, w]);
+  }
+
+  get x() {
+    return this[0];
+  }
+  get y() {
+    return this[1];
+  }
+  get z() {
+    return this[2];
+  }
+  get w() {
+    return this[3];
+  }
+  get is_a_point() {
+    return Math.round(this[3]) == 1;
+  }
+  get is_a_vector() {
+    return Math.round(this[3]) == 0;
+  }
+
+  tupleEqual(other: Tuple) {
+    return (
+      floatEqual(this.x, other.x) &&
+        floatEqual(this.y, other.y) &&
+        floatEqual(this.z, other.z) &&
+        floatEqual(this.w, other.w))
+  }
+
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export class Point extends Tuple {
+  constructor (x: number, y: number, z: number) {
+    super(x,y,z,1.0);
+  }
 }
+
+export class Vector extends Tuple {
+  constructor (x: number, y: number, z: number) {
+    super(x,y,z,0.0);
+  }
+}
+
+
+function floatEqual(a: number, b: number) : boolean {
+  return Math.abs(a-b) < EPSILON;
+}
+
+
+// // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+// if (import.meta.main) {
+//   console.log("Add 2 + 3 =", add(2, 3));
+// }
+
+
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,91 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { describe, it } from "@std/testing/bdd";
+import { assert, assertFalse, assertEquals, assertAlmostEquals } from "@std/assert";
+import { EPSILON, Tuple, Point, Vector } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+describe("tuples.feature", () => {
+
+  it("a tuple with w=1.0 is a point", () => {
+    //given
+    const a = new Tuple(4.3, -4.2, 3.1, 1.0)
+    //then
+    assertAlmostEquals(a.x, 4.3, EPSILON)
+    assertAlmostEquals(a.y,-4.2, EPSILON)
+    assertAlmostEquals(a.z, 3.1, EPSILON)
+    assertAlmostEquals(a.w, 1.0, EPSILON)
+    assert(a.is_a_point)
+    assertFalse(a.is_a_vector)
+  });
+
+  it("a tuple with w=0.0 is a vector", () => {
+    //given
+    const a = new Tuple(4.3, -4.2, 3.1, 0.0)
+    //then
+    assertAlmostEquals(a.x, 4.3, EPSILON)
+    assertAlmostEquals(a.y, -4.2, EPSILON)
+    assertAlmostEquals(a.z, 3.1, EPSILON)
+    assertAlmostEquals(a.w, 0.0, EPSILON)
+    assertFalse(a.is_a_point)
+    assert(a.is_a_vector)
+  });
+
+
+  it("a Point is a tuple with w=1", () => {
+    //given
+    const a = new Point(1.1, 2.2, 3.3)
+    //then
+    assert(a.tupleEqual(new Tuple(1.1, 2.2, 3.3, 1.0)))
+  });
+
+  it("a Vector is a tuple with w=0", () => {
+    //given
+    const a = new Vector(0.1, 0.2, 0.3)
+    //then
+    assert(a.tupleEqual(new Tuple(0.1, 0.2, 0.3, 0.0)))
+  });
+
+
+  it("a Vector is not equal to a Point with the same coords", () => {
+    //given
+    const v = new Vector(0.1, 0.2, 0.3)
+    const p = new Point(0.1, 0.2, 0.3)
+    //then
+    assertFalse(v.tupleEqual(p))
+  });
+
+  it("two Points are equal if they have the same components", () => {
+    //given
+    const v1 = new Point(4, 5, 6)
+    const v2 = new Point(4, 5, 6)
+    //then
+    assert(v1.tupleEqual(v2))
+  });
+
+  it("two Points are NOT equal if they have differing components", () => {
+    //given
+    const v1 = new Point(4, 5, 6.1)
+    const v2 = new Point(4, 5, 6)
+    //then
+    assertFalse(v1.tupleEqual(v2))
+  });
+
+
+  it("two Vectors are equal if they have the same components", () => {
+    //given
+    const v1 = new Vector(4, 5, 6)
+    const v2 = new Vector(4, 5, 6)
+    //then
+    assert(v1.tupleEqual(v2))
+  });
+
+  it("two Vectors are NOT equal if they have differing components", () => {
+    //given
+    const v1 = new Vector(4, 5, 6.1)
+    const v2 = new Vector(4, 5, 6)
+    //then
+    assertFalse(v1.tupleEqual(v2))
+  });
+
+
+
+
 });
