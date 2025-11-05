7c9d9b55d1721ee7d76a48520e585b669d50fc56
Simon Zelazny <zelazny@mailbox.org>
2025-11-05 13:44:39 +0100

    Don't mutate rotation matrix

--- clock.ts
+++ clock.ts
@@ -11,12 +11,12 @@ if (import.meta.main) {
   const shift = Matrix.identity()
     .scale(w / 4, h / 4, 0)
     .translate(w / 2, h / 2, 0);
-  let rotation = Matrix.identity();
 
   for (let hour = 0; hour < 12; hour++) {
-    const p = shift.times(rotation.times(new Point(0, -1, 0)));
+    const p = shift.times(Matrix.rotationZ(hourAngle * hour)).times(
+      new Point(0, -1, 0),
+    );
     c.setAt(Math.round(p.x), Math.round(p.y), white);
-    rotation = rotation.rotateZ(hourAngle);
   }
 
   await Deno.writeTextFile("clock.ppm", c.toPpm());
