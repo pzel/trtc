f979e4c3efa7569f140a8ecf88bcb07d01c15c5f
Simon Zelazny <zelazny@mailbox.org>
2025-11-06 09:22:43 +0100

    CH5 pp 69-70 (intersecting with transformed spheres)

--- sphere.ts
+++ sphere.ts
@@ -1,9 +1,17 @@
 import { Ray } from "./ray.ts";
 import { Point } from "./tuple.ts";
 import { Intersection, Intersections } from "./intersection.ts";
+import { Matrix } from "./matrix.ts";
 
 export class Sphere {
-  intersect(r: Ray): Intersections {
+  transform: Matrix;
+
+  constructor() {
+    this.transform = Matrix.identity();
+  }
+
+  intersect(r0: Ray): Intersections {
+    const r = r0.transform(this.transform.inverse());
     const sphereToRay = r.origin.minus(new Point(0, 0, 0));
     const a = r.direction.dot(r.direction);
     const b = 2 * r.direction.dot(sphereToRay);
