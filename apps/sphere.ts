203ede540cae668563bb35325378ee44e01928d2
Simon Zelazny <zelazny@mailbox.org>
2025-11-09 11:39:32 +0100

    Rewrite sphere.ts app according to hints

--- apps/sphere.ts
+++ apps/sphere.ts
@@ -7,24 +7,30 @@ import { Color } from "../color.ts";
 import { Matrix } from "../matrix.ts";
 
 if (import.meta.main) {
-  const w = 400, h = 400;
-  const c = new Canvas(w, h);
-  //  const wallZ = 10;
-  const s = 120;
+  const canvasPixels = 100;
+  const c = new Canvas(canvasPixels, canvasPixels);
+
+  const wallSize = 8;
+  const half = wallSize / 2;
+  const pixelSize = wallSize / canvasPixels; // 0.08?
+  const rayOrigin = new Point(0, 0, -5);
+  const wallZ = 10;
 
   const sphere = new Sphere();
-  sphere.transform = Matrix
-    .scaling(s, s, s)
-    .shear(0.5, 0, 0, 0, 0, 0)
-    .rotateX(Math.PI / 6)
-    .translate(w / 2, h / 2, 0);
-  for (let i = 0; i < w; i++) {
-    for (let j = 0; j < h; j++) {
-      const r = new Ray(new Point(i, j, -10), new Vector(0, 0, 1));
+  sphere.transform = Matrix.identity();
+  //    .translate(canvasPixels / 2, canvasPixels / 2, 0);
+
+  for (let y = 0; y < canvasPixels - 1; y++) {
+    const worldY = half - (pixelSize * y);
+    for (let x = 0; x < canvasPixels - 1; x++) {
+      const worldX = half - (pixelSize * x);
+
+      const positionOnWall = new Point(worldX, worldY, wallZ);
+      const r = new Ray(rayOrigin, positionOnWall.minus(rayOrigin).normalize());
       const hit = sphere.intersect(r).hit();
       if (hit) {
-        const gray = (hit.t * 2) / 255;
-        c.setAt(i, j, new Color(gray, gray, gray));
+        const gray = 1 - (hit.t / 7.0);
+        c.setAt(x, y, new Color(gray, gray, gray));
       }
     }
   }
