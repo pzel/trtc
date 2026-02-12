a546561a30784271f4ca6ae820af7febf3861371
Simon Zelazny <zelazny@mailbox.org>
2026-02-12 21:37:47 +0100

    Re-add forgotten file (testing.ts)

---
+++ testing.ts
@@ -0,0 +1,11 @@
+export { assert, assertEquals, assertStrictEquals } from "@std/assert";
+export { describe, it } from "@std/testing/bdd";
+
+import { EPSILON } from "./float.ts";
+import { assert, assertAlmostEquals } from "@std/assert";
+
+export function assertFloatEquals(a: number | undefined, b: number | undefined) {
+  assert(a);
+  assert(b);
+ 	assertAlmostEquals(a, b, EPSILON);
+}
